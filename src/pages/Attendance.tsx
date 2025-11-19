2dccbb3ad33012f201e708af740fe2b9a25ad15d
zizo2762zaza-creator <zizo2762zaza@gmail.com>
2025-11-19 23:18:44 +0100

    Fix API import paths for Vercel

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import api from "../services/api";
+import { api } from "../../services/api";
 import { Link } from "react-router-dom";
 
 export default function Attendance() {
